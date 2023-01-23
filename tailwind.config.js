f1d4950ba5ac64947ca975b05fbf0f0239cbc8ef
Raul Battistini <raulbattistini.3@gmail.com>
2023-01-23 10:53:01 -0300

    feat: start next app

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      pocket: { min: "280px", max: "479px" },
+      phone: { min: "480px", max: "639px" },
+      tablet: { min: "640px", max: "767px" },
+      laptop: { min: "768px", max: "1023px" },
+      desktop: { min: "1024px", max: "1279px" },
+      ultrawide: { min: "1280px", max: "9999px" },
+    },
+    fontFamily: {
+      poppins: ['poppins, "sans-serif"'],
+    },
+    extend: {
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+      colors: {
+        "black-rgba": "rgba(0, 0, 0, 0.5)",
+        "transparent": "rgba(255, 255, 255, 0)",
+        "glass-white": "rgba(255, 255, 255, 0.1)",
+        "sea-green-rgba": "rgba(4, 104, 101, 0.5)"
+      },
+    },
+  },
+  plugins: [],
+};
